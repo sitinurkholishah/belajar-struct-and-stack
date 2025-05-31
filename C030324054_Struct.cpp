87815388a8ddf2348e52f6ec5884ea1b0483a28c
Siti Nurkholishah <situnurkholishah7@gmail.com>
2025-05-31 14:02:36 +0800

    prosedur inputdata pointer

--- C030324054_Struct.cpp
+++ C030324054_Struct.cpp
@@ -9,6 +9,17 @@ struct mahasiswa {
     float ipk;
 };
 
+void inputdata(mahasiswa *prodi_TI){
+    cout << "masukkan nama : ";
+    cin >> prodi_TI->nama;
+    cout << "masukkan nim : ";
+    cin >> prodi_TI->nim;
+    cout << "masukkan alamat : ";
+    cin >> prodi_TI->alamat;
+    cout << "masukkan ipk : ";
+    cin >> prodi_TI->ipk;
+}
+
 int main() {
     mahasiswa Siti_Nurkholishah;
 
@@ -17,10 +28,10 @@ int main() {
     Siti_Nurkholishah.alamat = "Jl. Sungai Andai Komp. kesehatan Blok.H No.13";
     Siti_Nurkholishah.ipk = 3.48;
 
-    cout << Siti_Nurkholishah.nim << "\n";
-    cout << Siti_Nurkholishah.nama << "\n";
-    cout << Siti_Nurkholishah.alamat << "\n";
-    cout << Siti_Nurkholishah.ipk << "\n";
+    cout <<"Nama: " << Siti_Nurkholishah.nama << "\n";
+    cout <<"Nim: " << Siti_Nurkholishah.nim << "\n";
+    cout <<"Alamat: "<< Siti_Nurkholishah.alamat << "\n";
+    cout <<"ipk: "<<Siti_Nurkholishah.ipk << "\n";
 
     return 0;
 }
