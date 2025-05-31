4f4a174036aefcc4d86e7e59496bcf9d6029c558
Siti Nurkholishah <situnurkholishah7@gmail.com>
2025-05-31 16:41:27 +0800

    menambahkan prosedur pop

--- C030324054_stack.cpp
+++ C030324054_stack.cpp
@@ -20,3 +20,13 @@ int Full(Stack *S){
 int Empty(Stack *S){
     return (S->Count == 0);
 }
+
+void Pop(Stack *S, stokItem *x){
+    if (Empty(S))
+        cout << "Gudang masih kosong!" << endl;
+    else{
+        --(S->Count);
+        *x = S->Barang[S->Count];
+        cout << "Barang dengan ID " << *x << " berhasil diambil dari gudang" << endl;
+    }
+}
